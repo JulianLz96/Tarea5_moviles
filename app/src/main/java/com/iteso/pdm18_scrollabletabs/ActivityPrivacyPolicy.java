86583f77bf7c04c5b241e8c1301784cd27de65a7
JulianLz96 <jj_lz96@hotmail.com>
2018-10-07 19:45:18 -0500

    MyWebView implemented

--- app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityPrivacyPolicy.java
+++ app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityPrivacyPolicy.java
@@ -1,9 +1,12 @@
 package com.iteso.pdm18_scrollabletabs;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -22,6 +25,17 @@ public class ActivityPrivacyPolicy extends AppCompatActivity {
             Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
         }
     }
+    private class MyWebViewCLient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("www.iteso.mx")) {
+                return false;
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +46,7 @@ public class ActivityPrivacyPolicy extends AppCompatActivity {
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
-        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebViewClient(new MyWebViewCLient());
         mWebView.loadUrl("file:///android_asset/PrivacyPolicy.html");
     }
 }
