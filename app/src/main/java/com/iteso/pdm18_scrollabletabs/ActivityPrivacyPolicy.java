831454954b2403ee5a66f151590372bd1e135b16
JulianLz96 <jj_lz96@hotmail.com>
2018-10-07 19:18:55 -0500

    WebClient inception

--- app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityPrivacyPolicy.java
+++ app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityPrivacyPolicy.java
@@ -1,11 +1,27 @@
 package com.iteso.pdm18_scrollabletabs;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class ActivityPrivacyPolicy extends AppCompatActivity {
-
+        /**Inner class*/
+     class WebAppInterface {
+        Context mContext;
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+        /**Show a toast from the web page*/
+        @JavascriptInterface
+            public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +29,10 @@ public class ActivityPrivacyPolicy extends AppCompatActivity {
         WebView mWebView = null;
             mWebView = findViewById(R.id.activity_privacy_policy_webView);
 
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        mWebView.setWebViewClient(new WebViewClient());
         mWebView.loadUrl("file:///android_asset/PrivacyPolicy.html");
     }
 }
